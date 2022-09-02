39f6a876c0d2ca60168942d60e0e1437cd37c7d2
UzoGaMuzol <uzogamuzol@gmail.com>
2022-09-02 18:56:33 +0900

    add content

--- index.py
+++ index.py
@@ -1,7 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, url_for, render_template
 
 app = Flask(__name__)
 
+from api import blueprint as api_bp
+app.register_blueprint(api_bp)
+
 @app.route("/")
 def index():
-    return jsonify({"test":"Hello, World!"})
+    return render_template("index.html")
