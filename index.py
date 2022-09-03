10e24971a06f0ca6171074b017fae36425355d80
UzoGaMuzol <uzogamuzol@gmail.com>
2022-09-03 11:29:42 +0900

    日本語&CORS

--- index.py
+++ index.py
@@ -1,6 +1,13 @@
-from flask import Flask, jsonify, url_for, render_template
+from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(
+    app,
+    supports_credentials = True
+)
+
+app.config["JSON_AS_ASCII"] = False
 
 from api import blueprint as api_bp
 app.register_blueprint(api_bp)
